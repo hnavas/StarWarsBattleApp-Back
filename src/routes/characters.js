90b3277c2ad0ea754213a465b73ff5400b33f7a5
hnavas <hendri.navas@gmail.com>
2023-02-07 17:40:33 -0300

    First Commit Back

---
+++ src/routes/characters.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const axios = require("axios");
+
+const getCharacters = async () => {
+  const allCharacters = [];
+  const dataApi = await axios.get('https://akabab.github.io/starwars-api/api/all.json');
+  const getData = dataApi.data.map(ch => {
+    const allData = {
+      id: ch.id,
+      name: ch.name,
+      image: ch.image,
+      gender: ch.gender,
+      died: ch.died,
+      species: ch.species
+    }
+    allCharacters.push(allData);
+  })
+  return allCharacters;
+}
+
+router.get('/', async (req, res) => {
+  let all = await getCharacters();
+  res.status(200).json(all);
+})
+
+module.exports = router;
---
+++ src/routes/index.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+const characters = require('./characters');
+
+const router = Router();
+
+router.use('/characters', characters);
+
+module.exports = router;
